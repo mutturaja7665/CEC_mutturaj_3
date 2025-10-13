6321858b65ecd3ff739ef7ae47cca105eff45cdb
mutturaja7665 <mutturaja7665@gmail.com>
2025-10-13 16:01:18 +0530

    Add files via upload

---
+++ Day4/JS/prg1.js
@@ -0,0 +1,24 @@
+var msg = "I am Dhanush";
+        console.log(msg);
+
+        let mssg = "I am Dhanush";
+         console.log(mssg);
+
+
+         const greeting = {
+         text: "Welcome!",
+            sender: "System"
+            };
+             console.log(greeting);
+
+          function hellofunction(){
+               console.log("hello from function");
+          }
+
+          hellofunction();
+
+
+          helloArrowFunction = () => {
+              console.log('Hello From Arrow Function');
+          }
+          helloArrowFunction();
